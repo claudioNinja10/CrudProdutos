e4d2bbd71733b1039fb3213e5b9a169e3b7c7150
Claudio Calipi <165864381+claudioNinja10@users.noreply.github.com>
2024-07-03 21:49:43 -0300

    Create scprit.js

---
+++ scprit.js
@@ -0,0 +1,25 @@
+let nome = document.getElementById("produtoName")
+let descricao = document.getElementById("produDescricao")
+let valor = document.getElementById("Valor")
+
+
+let produtos = []
+
+function cadastrar(){
+    let produto = {
+
+        nome: nome.value,
+        descricao: descricao.value,
+        preco: Number(preco.value)
+    }
+
+    produtos.push(produto)
+    console.log(produto)
+
+
+    nome.value = ""
+    descricao.value = ""
+    preco.value = ""
+    nome.focus()
+
+}
